7114e4b4320a50253b1041bec6d946e8a1e49386
Avi Romanoff <avi@romanoff.me>
2016-04-10 21:56:23 -0400

    refactor: better names for files

--- hash.go
+++ hash.go
@@ -8,8 +8,6 @@ import (
 func HashString(str string) string {
 	hash := sha256.New()
 	hash.Write([]byte(str))
-	md := hash.Sum(nil)
-	hashedStr := hex.EncodeToString(md)
 
-	return hashedStr
+	return hex.EncodeToString(hash.Sum(nil))
 }
