f5970d6f33e3d6648817db91bfaabdab1888e7af
Jordan Brown <jmbrown@andrew.cmu.edu>
2016-04-12 23:45:07 -0400

    renamed num to number

--- models/character.go
+++ models/character.go
@@ -4,7 +4,7 @@ package models
 type Character struct {
 	ID         string `bson:"_id" json:"id"`
 	ProID      string `bson:"pro_id" json:"pro_id"`
-	PlayerNum  int    `bson:"num" json:"num"`
+	PlayerNum  int    `bson:"number" json:"number"`
 	Experience int    `bson:"experience" json:"experience"`
 	Race string `bson:"race" json:"race"`
 	Strength int `bson:"strength" json:"strength"`
