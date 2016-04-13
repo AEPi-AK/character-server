077794c04c5d151e98c61cfd39c45bac9870d6ba
Jordan Brown <jmbrown@andrew.cmu.edu>
2016-04-12 23:46:06 -0400

    renamed experience to points in character.go

--- models/character.go
+++ models/character.go
@@ -5,7 +5,7 @@ type Character struct {
 	ID         string `bson:"_id" json:"id"`
 	ProID      string `bson:"pro_id" json:"pro_id"`
 	PlayerNum  int    `bson:"number" json:"number"`
-	Experience int    `bson:"experience" json:"experience"`
+	Experience int    `bson:"points" json:"points"`
 	Race string `bson:"race" json:"race"`
 	Strength int `bson:"strength" json:"strength"`
 	Dexterity int `bson:"dexterity" json:"dexterity"`
